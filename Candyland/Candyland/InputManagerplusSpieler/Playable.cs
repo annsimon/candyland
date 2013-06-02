082ee2d673c6010a8ff92bd897a41dd1de8e3553
gollabaer <bjoern.golla@gmx.de>
2013-06-02 14:15:01 +0200

    *adjusted Varaibles to GameObject *Changed Jump funtion

--- Candyland/Candyland/InputManagerplusSpieler/Playable.cs
+++ Candyland/Candyland/InputManagerplusSpieler/Playable.cs
@@ -12,16 +12,14 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Candyland
 {
-    abstract class Playable
+    abstract class Playable : GameObject
     {
         protected Camera cam;               //Kamera
-        protected Vector3 position;         //Position des Spielers
         protected Vector3 direction;        //Laufrichtung des Spielers x-z Ebene
         protected float currentspeed;       //Momentane geschwindigkeit
         protected float gravity;            //beschleinigungsfaktor in y richtung  
         protected float upvelocity;         //beschleinigungsfaktor un y richtung
         protected Model model;              //CharacterModel
-        protected BoundingBox bbox;         //HitBox
 
         abstract public void loadContent(ContentManager content);
 
@@ -31,19 +29,29 @@ namespace Candyland
 
         public abstract void moveTo(Vector3 goalpoint);
 
+        /// <summary>
+        /// Moves the Player and the Camera
+        /// </summary>
+        /// <param name="movex">movement in Camera - X Coordinates</param>
+        /// <param name="movey">movement in Camera - Y Coordinates</param>
+        /// <param name="camx">Sideways rotation of the Camera</param>
+        /// <param name="camy">Up / Down Rotation of the Camera</param>
         public abstract void movementInput(float movex, float movey, float camx, float camy);
 
 
 
-
-        public  Vector3 getPosition(){ return position; }
-
-        // todo: FIX THIS!
+        /// <summary>
+        /// Returns a float representing the CameraAngle
+        /// </summary>
+        /// <returns></returns>
         public float getCameraDir() { return cam.getDirection(); }
 
         public  Matrix getProjectionM(){return cam.getProjectionMatrix();}
 
         public Matrix getViewM() { return cam.getviewMatrix(); }
+
+
+
 
         /// <summary>
         /// Moves the Player, input should be between [-1,1]
@@ -54,19 +62,14 @@ namespace Candyland
         {
             if (x != 0 && y != 0)
             {
-                float length = (float)Math.Sqrt(x * x + y * y);
-                direction = new Vector3(x / length, 0, y / length);
-                //TODO Länge normalisieren
-                currentspeed = length * 0.1f;
-                position += direction * currentspeed;
-                cam.changeposition(position);
+                float length = (float)Math.Sqrt(x * x + y * y);     //Calculate length of MovementVector
+                direction = new Vector3(x, 0, y);                   //Movement Vector
+                direction.Normalize();                              //Normalize MovementVector
+                currentspeed = length * 0.1f;                       //Scale MovementVector for different walking speeds
+                m_position += direction * currentspeed;             //Change PLayerPosition
+                cam.changeposition(m_position);                     //Change CameraPosition
             }
         }
-
-
-        public  BoundingBox getBoundingBox() { return bbox; }
-
-
 
 
 
