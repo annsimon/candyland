193bd9faaa40f6abad4d3cca14ef5fc29b2951b2
Sebro <sebastianrohde91@gmx.de>
2013-06-15 20:46:42 +0200

    Input for SpurWechsel Level + SpurwechselTest.xml

--- SceneEditor/SceneEditor/Scene.cs
+++ SceneEditor/SceneEditor/Scene.cs
@@ -28,41 +28,53 @@ namespace SceneEditor
 
         private void addAreaButton_Click(object sender, EventArgs e)
         {
-            m_areaGenerator.InitializeNewArea();
-            if (m_areaGenerator.ShowDialog() == DialogResult.OK)
+            try
             {
-                Area area = new Area();
-                area.id = m_areaGenerator.id;
-                area.idNext = m_areaGenerator.idNext;
-                area.idPrev = m_areaGenerator.idPrev;
-                area.posX = m_areaGenerator.posX;
-                area.posY = m_areaGenerator.posY;
-                area.posZ = m_areaGenerator.posZ;
+                m_areaGenerator.InitializeNewArea();
+                if (m_areaGenerator.ShowDialog() == DialogResult.OK)
+                {
+                    Area area = new Area();
+                    area.id = m_areaGenerator.id;
+                    area.idNext = m_areaGenerator.idNext;
+                    area.idPrev = m_areaGenerator.idPrev;
+                    area.posX = m_areaGenerator.posX;
+                    area.posY = m_areaGenerator.posY;
+                    area.posZ = m_areaGenerator.posZ;
 
-                area.levels = new List<Level>();
-                foreach (Level lvl in m_areaGenerator.levels)
-                    area.levels.Add(lvl);
+                    area.levels = new List<Level>();
+                    foreach (Level lvl in m_areaGenerator.levels)
+                        area.levels.Add(lvl);
 
-                m_areas.Add(area);
-                listBox1.Items.Add(area);
+                    m_areas.Add(area);
+                    listBox1.Items.Add(area);
+                }
+            }
+            catch
+            {
             }
         }
 
         private void editAreaButton_Click(object sender, EventArgs e)
         {
-            Area current = (Area)listBox1.SelectedItem;
-            m_areaGenerator.InitializeWithArea(current);
-            if (m_areaGenerator.ShowDialog() == DialogResult.OK)
+            try
             {
-                current.id = m_areaGenerator.id;
-                current.idNext = m_areaGenerator.idNext;
-                current.idPrev = m_areaGenerator.idPrev;
-                current.posX = m_areaGenerator.posX;
-                current.posY = m_areaGenerator.posY;
-                current.posZ = m_areaGenerator.posZ;
-                current.levels = new List<Level>();
-                foreach (Level lvl in m_areaGenerator.levels)
-                    current.levels.Add(lvl);
+                Area current = (Area)listBox1.SelectedItem;
+                m_areaGenerator.InitializeWithArea(current);
+                if (m_areaGenerator.ShowDialog() == DialogResult.OK)
+                {
+                    current.id = m_areaGenerator.id;
+                    current.idNext = m_areaGenerator.idNext;
+                    current.idPrev = m_areaGenerator.idPrev;
+                    current.posX = m_areaGenerator.posX;
+                    current.posY = m_areaGenerator.posY;
+                    current.posZ = m_areaGenerator.posZ;
+                    current.levels = new List<Level>();
+                    foreach (Level lvl in m_areaGenerator.levels)
+                        current.levels.Add(lvl);
+                }
+            }
+            catch
+            {
             }
         }
 
