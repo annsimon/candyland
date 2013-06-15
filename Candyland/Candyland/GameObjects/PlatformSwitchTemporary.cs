8cca4c2f48fe55ede1847244f07b924a62a6007e
Sebro <sebastianrohde91@gmx.de>
2013-06-15 14:46:20 +0200

    PlattformSwitch Model & Texture

--- Candyland/Candyland/GameObjects/PlatformSwitchTemporary.cs
+++ Candyland/Candyland/GameObjects/PlatformSwitchTemporary.cs
@@ -27,7 +27,7 @@ namespace Candyland
             this.m_texture = content.Load<Texture2D>("schaltertextur");
             this.m_original_texture = this.m_texture;
             this.effect = content.Load<Effect>("Toon");
-            this.m_model = content.Load<Model>("plattformschalter");
+            this.m_model = content.Load<Model>("schalterplattform");
             this.m_original_model = this.m_model;
 
             this.calculateBoundingBox();
