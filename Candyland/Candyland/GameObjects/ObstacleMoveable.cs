32c13fe51829fe65610f6e7fb56437f50ebea935
Sebro <sebastianrohde91@gmx.de>
2013-06-16 16:09:25 +0200

    Block edited

--- Candyland/Candyland/GameObjects/ObstacleMoveable.cs
+++ Candyland/Candyland/GameObjects/ObstacleMoveable.cs
@@ -34,10 +34,10 @@ namespace Candyland
 
         public override void load(ContentManager content)
         {
-            this.m_texture = content.Load<Texture2D>("wunderkugeltextur");
+            this.m_texture = content.Load<Texture2D>("blocktextur");
             this.m_original_texture = this.m_texture;
             this.effect = content.Load<Effect>("Toon");
-            this.m_model = content.Load<Model>("wunderkugelmovable");
+            this.m_model = content.Load<Model>("blockmovable");
             this.m_original_model = this.m_model;
 
             this.calculateBoundingBox();
