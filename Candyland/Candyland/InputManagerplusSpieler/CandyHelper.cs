1748dc85a2dbb38c944426fdebd5ccd210e699d6
Svenja <svenja.handreck@gmx.net>
2013-06-17 11:24:14 +0200

    some more collision restructuring
    playable handles chocolatechip collision now and cam.collidewith is called in collide() within playable

--- Candyland/Candyland/InputManagerplusSpieler/CandyHelper.cs
+++ Candyland/Candyland/InputManagerplusSpieler/CandyHelper.cs
@@ -102,32 +102,6 @@ namespace Candyland
                 obj.isNotCollidingWith(this);
             }
         }
-        //private void collideWithMovable(GameObject obj)
-        //{
-        //    if (obj.getBoundingBox().Intersects(m_boundingBox))
-        //    {
-        //        preventIntersection(obj);
-        //        if(minOld.Y < obj.getBoundingBox().Max.Y) // Do we need this here?
-        //        obj.hasCollidedWith(this);
-        //    }
-        //    else
-        //    {
-        //        obj.isNotCollidingWith(this);
-        //    }
-        //}
-
-        // Needs to be able to collect the chips
-        protected override void collideWithChocoChip(GameObject obj)
-        {
-            if (obj.getBoundingBox().Intersects(m_boundingBox))
-            {
-                obj.hasCollidedWith(this);
-            }
-            else
-            {
-                obj.isNotCollidingWith(this);
-            }
-        }
 
         #endregion
 
