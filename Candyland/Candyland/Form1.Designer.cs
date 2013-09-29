8e049543dc94b5cf0423ecd02f8dfe343a045450
Svenja <svenja.handreck@gmx.net>
2013-09-29 11:56:39 +0200

    window size changed, cursor hidden, window name changed

--- Candyland/Candyland/Form1.Designer.cs
+++ Candyland/Candyland/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.surface.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.surface.Location = new System.Drawing.Point(0, -3);
+            this.surface.Location = new System.Drawing.Point(0, 0);
             this.surface.Name = "surface";
-            this.surface.Size = new System.Drawing.Size(285, 265);
+            this.surface.Size = new System.Drawing.Size(800, 480);
             this.surface.TabIndex = 0;
             this.surface.TabStop = false;
             // 
@@ -47,13 +47,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(800, 480);
             this.Controls.Add(this.surface);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Save the Sweets";
             ((System.ComponentModel.ISupportInitialize)(this.surface)).EndInit();
             this.ResumeLayout(false);
 
+            // Getting rid of the cursor, no idea if doing it this way is good, but seems to work
+            this.Cursor.Dispose();
         }
 
         #endregion
