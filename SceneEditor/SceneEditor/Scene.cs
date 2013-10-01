b29cb91c47c2616acea5ba021458fba33ecbcd3f
Svenja <svenja.handreck@gmx.net>
2013-10-01 18:09:20 +0200

    Scene Manager set back to theWorld and removed BB Level

--- SceneEditor/SceneEditor/Scene.cs
+++ SceneEditor/SceneEditor/Scene.cs
@@ -17,7 +17,7 @@ namespace SceneEditor
         List<Area> m_areas;
         AreaGenerator m_areaGenerator;
 
-        string currentFile = "BalanceBoardWorld.xml";
+        string currentFile = "theWorld.xml";
 
         public SceneGenerator()
         {
